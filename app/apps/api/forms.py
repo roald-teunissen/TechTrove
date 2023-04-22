f1342ed37e307af0c7acd7eff6d5f3480da49e2d
Roald Teunissen <roaldteunissen@gmail.com>
2023-04-22 13:58:08 +0200

    Initial database schema, forms and models

--- app/apps/api/forms.py
+++ app/apps/api/forms.py
@@ -1,9 +1,10 @@
 from wtforms import Form
 from wtforms_alchemy import model_form_factory
-
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, TextAreaField
+from wtforms.validators import DataRequired
 
 from apps.models import *
-
 
 ModelForm = model_form_factory(Form)
 
@@ -11,4 +12,43 @@ ModelForm = model_form_factory(Form)
 class BookForm(ModelForm):
     class Meta:
         model = Book
+
+class ManufacturerForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+
+class ProductCategoryForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+
+class VendorForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    website = StringField('Website')
+
+class ProductForm(FlaskForm):
+    model = StringField('Model', validators=[DataRequired()])
+    quantity = IntegerField('Quantity', validators=[DataRequired()])
+    price_when_bought = IntegerField('Price when bought', validators=[DataRequired()])
+    description = TextAreaField('Description')
+    EAN = StringField('EAN')
+    manufacturer = StringField('Manufacturer', validators=[DataRequired()])
+    category = StringField('Category', validators=[DataRequired()])
+    vendor = StringField('Vendor', validators=[DataRequired()])
+
+class UserForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
+
+class BorrowForm(FlaskForm):
+    product_id = IntegerField('Product ID', validators=[DataRequired()])
+    user_id = IntegerField('User ID', validators=[DataRequired()])
+    quantity = IntegerField('Quantity', default=1, validators=[DataRequired()])
+    returned = IntegerField('Returned', default=0, validators=[DataRequired()])
+    estimated_return_date = IntegerField('Estimated return date', default=0, validators=[DataRequired()])
+    borrowed_at = IntegerField('Borrowed at', validators=[DataRequired()])
+
+class OrderedForm(FlaskForm):
+    product_id = IntegerField('Product ID', validators=[DataRequired()])
+    vendor_id = IntegerField('Vendor ID', validators=[DataRequired()])
+    ordered = IntegerField('Ordered', default=0, validators=[DataRequired()])
+    delivered = IntegerField('Delivered', default=0, validators=[DataRequired()])
+    ordered_at = IntegerField('Ordered at', validators=[DataRequired()])
 
--- app/apps/models.py
+++ app/apps/models.py
@@ -5,11 +5,62 @@ Copyright (c) 2019 - present AppSeed.us
 
 from apps import db
 
-'''
-Add your models below
-'''
+class Manufacturer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
 
-# Book Sample
+class ProductCategory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+
+class Vendor(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    website = db.Column(db.String(255))
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    model = db.Column(db.String(255), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    price_when_bought = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.Text)
+    EAN = db.Column(db.String(255))
+    created_at = db.Column(db.Integer, nullable=False)
+    updated_at = db.Column(db.Integer, nullable=False)
+    manufacturer_id = db.Column(db.Integer, db.ForeignKey('manufacturer.id'))
+    category_id = db.Column(db.Integer, db.ForeignKey('product_category.id'))
+    vendor_id = db.Column(db.Integer, db.ForeignKey('vendor.id'))
+    manufacturer = db.relationship('Manufacturer', backref='products')
+    category = db.relationship('ProductCategory', backref='products')
+    vendor = db.relationship('Vendor', backref='products')
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    created_at = db.Column(db.Integer, nullable=False)
+    updated_at = db.Column(db.Integer, nullable=False)
+
+class Borrow(db.Model):
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    quantity = db.Column(db.Integer, nullable=False, default=1)
+    returned = db.Column(db.Integer, nullable=False, default=0)
+    estimated_return_date = db.Column(db.Integer, nullable=False, default=0)
+    borrowed_at = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.Integer, nullable=False)
+    product = db.relationship('Product', backref='borrows')
+    user = db.relationship('User', backref='borrows')
+
+class Ordered(db.Model):
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), primary_key=True)
+    vendor_id = db.Column(db.Integer, db.ForeignKey('vendor.id'), primary_key=True)
+    ordered = db.Column(db.Integer, nullable=False, default=0)
+    delivered = db.Column(db.Integer, nullable=False, default=0)
+    ordered_at = db.Column(db.Integer, nullable=False)
+    product = db.relationship('Product', backref='orders')
+    vendor = db.relationship('Vendor', backref='orders')
+
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(64))
